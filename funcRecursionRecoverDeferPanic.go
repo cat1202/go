f7ced9db1e65bbc656f6978114383bd37b2412e5
cat1202 <583480751@qq.com>
2019-01-01 21:06:40 +0800

    function return varible

--- funcRecursionRecoverDeferPanic.go
+++ funcRecursionRecoverDeferPanic.go
@@ -17,19 +17,24 @@ func addThemUp(numbers []float64) float64 {
 	return sum
 }
 
+// Go functions can return multiple values
+func next2Values(number int) (int, int, bool) {
+
+	return number + 1, number + 2, true
+
+}
 func main() {
 
 	listOfNums := []float64{1, 2, 3}
 	fmt.Println("Sum :", addThemUp(listOfNums))
-	return
-
+	//return
 	// Get 2 values from a function
-	num1, num2 := next2Values(5)
-
-	fmt.Println(num1, num2)
+	num1, num2, _ := next2Values(5)
+	_, _, resBool := next2Values(1)
+	fmt.Println(num1, num2, resBool)
+	//return //->6 7 true
 
 	// Send an undefined number of values to a function (Variadic Function)
-
 	fmt.Println(subtractThem(1, 2, 3, 4, 5))
 
 	// You can create a function in a function. It has access to the
@@ -71,16 +76,7 @@ func main() {
 
 }
 
-// Go functions can return multiple values
-
-func next2Values(number int) (int, int) {
-
-	return number + 1, number + 2
-
-}
-
 // You can receive an undefined number of values with args ...int
-
 func subtractThem(args ...int) int {
 
 	finalValue := 0
